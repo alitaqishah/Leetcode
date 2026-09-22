9e67efb89e42200b92b9fafaa6e06ecca646c8c7
Syed Ali Taqi <alitaqishah@gmail.com>
2026-09-22 22:09:48 +0500

    Time: 4 ms (14.18%) | Memory: 19.4 MB (66.42%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hIndex(self, citations):
+        citations.sort()
+
+        n = len(citations)
+
+        for i in range(n):
+            h = n - i
+
+            if citations[i] >= h:
+                return h
+
+        return 0
